a3bfb1d7ede7b49e47fa5d740d2879c45a4692ba
Yasset Perez-Riverol <ypriverol@gmail.com>
2015-11-09 17:40:40 +0000

    README

--- src/main/java/uk/ac/ebi/ddi/massive/extws/entrez/client/taxonomy/TaxonomyWsClient.java
+++ src/main/java/uk/ac/ebi/ddi/massive/extws/entrez/client/taxonomy/TaxonomyWsClient.java
@@ -2,8 +2,12 @@ package uk.ac.ebi.ddi.massive.extws.entrez.client.taxonomy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.util.UriUtils;
 import uk.ac.ebi.ddi.massive.extws.entrez.config.TaxWsConfigProd;
 import uk.ac.ebi.ddi.massive.extws.entrez.ncbiresult.NCBITaxResult;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 
 /**
@@ -22,9 +26,10 @@ public class TaxonomyWsClient extends WsClient {
         super(config);
     }
 
-    public NCBITaxResult getNCBITax(String term){
+    public NCBITaxResult getNCBITax(String term) throws UnsupportedEncodingException {
 
         if(term != null && term.length() > 0){
+            term = term.replace("#","").replace("%", "");
             String url = String.format("%s://%s/entrez/eutils/esearch.fcgi?db=taxonomy&term=%s&retmode=JSON",
                     config.getProtocol(), config.getHostName(), term);
             //Todo: Needs to be removed in the future, this is for debugging
---
+++ src/test/java/uk/ac/ebi/ddi/massive/extws/entrez/client/taxonomy/TaxonomyWsClientTest.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.ddi.massive.extws.entrez.client.taxonomy;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaxonomyWsClientTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNCBITax() throws Exception {
+
+    }
+}
